165bc37a03a2b7d9dcd15f07af6003f0fe0bb140
JohanesSimon <150922268+JohanesSimon@users.noreply.github.com>
2023-11-16 18:15:47 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,26 @@
+function p() {
+    var p = document.getElementById("p");
+    if (p.style.display === "none") {
+        p.style.display = "block";
+    } else {
+        p.style.display = "none";
+    }
+}
+
+function d() {
+    var d = document.getElementById("skills");
+    if (d.style.display === "none") {
+        d.style.display = "block";
+    } else {
+        d.style.display = "none";
+    }
+}
+
+function hide() {
+    var ham = document.getElementById("Harmal");
+    if (ham.style.display === "none") {
+        ham.style.display = "block";
+    } else {
+        ham.style.display = "none";
+    }
+}
